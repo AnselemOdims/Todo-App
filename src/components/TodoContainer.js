b159f7012b50c10c0c287048bc09527f7ff09086
Anselem <anselemodims@gmail.com>
2022-01-09 18:58:08 +0100

    Feat: created the TodoContainer class and included a simple render

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TodoContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    );
+  }
+}
+
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
