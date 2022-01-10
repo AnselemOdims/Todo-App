1e6f69ba835c8c3c9c55df5b5cf51f00346a51b6
Anselem <anselemodims@gmail.com>
2022-01-10 16:31:05 +0100

    Feat: created the About and NotMatch pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
+import About from './pages/About';
+import NotMatch from './pages/NotMatch';
 import './App.css';
 
 ReactDOM.render(
@@ -11,14 +13,8 @@ ReactDOM.render(
 		<Router>
 			<Routes>
         <Route path='/' element={<TodoContainer/>} />
-        <Route
-					path='*'
-					element={
-						<main style={{ padding: '1rem' }}>
-							<p>There's nothing here!</p>
-						</main>
-					}
-				/>
+        <Route path='/about' element={<About/>} />
+        <Route path='*' element={<NotMatch/>} />
 			</Routes>
 		</Router>
 	</React.StrictMode>,
---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+  return ( 
+    <div>hello from about page</div>
+   );
+}
+ 
+export default About;
