b7f0e7799072837027a214f16326b9ce2e87a65b
Anselem <anselemodims@gmail.com>
2022-01-10 10:46:01 +0100

    Feat: styled the todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
+import styles from './TodoItem.module.css';
 
 const TodoItem = (props) => {
   const { todo, handleChange, handleDelete } = props;
   return (
-    <li>
+    <li className={styles.item}>
       <input
         type="checkbox"
+        className={styles.checkbox}
         checked={todo.completed}
         onChange={() => handleChange(todo.id)}
       />
-      {' '}
-      {todo.title}
+      <span className={todo.completed ? styles.completedStyle : ''}>
+        {todo.title}
+      </span>
       <button type="button" onClick={() => handleDelete(todo.id)}>Delete</button>
     </li>
   );
